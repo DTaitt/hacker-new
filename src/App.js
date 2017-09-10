6048d65512034beea4a71a0bbac78eb74223c037
DTaitt <donovan.taitt@yahoo.com>
2017-09-10 15:15:21 -0400

    created StoryListContainer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 
 import Header from './components/Header';
+import StoryListContainer from './components/StoryListContainer';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <Header />
+          <StoryListContainer />
         </div>
       </Router>
     );
---
+++ src/components/StoryListContainer.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+import StoryList from './StoryList';
+
+
+
+class StoryListContainer extends Component {
+        
+    state = {
+        topStories: [],
+        //isTopStoriesLoaded: false,
+    }
+    
+
+    componentDidMount() {
+        this.fetchTopStories();
+    }
+
+    fetchTopStories() {
+        fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
+            .then(res => {
+                return res.json()
+            })
+            .then(jsonRes => {
+                //console.log(jsonRes)
+                jsonRes.forEach(id => {
+                    fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
+                        .then((storyRes) => {
+                            return storyRes.json()
+                        })
+                        .then(storyJson => {
+                            //console.log(storyJson)
+                            this.setState((prevState) => ({
+                                topStories: [...prevState.topStories, storyJson]
+                            }))
+                        })
+                })
+            })
+    }
+    
+    render() {
+        
+        return(
+            
+            <p>rendering storylist container </p>        
+        );
+    }
+}
+
+export default StoryListContainer;
