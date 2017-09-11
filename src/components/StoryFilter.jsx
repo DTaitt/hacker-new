c514682a18c185a307bae80377a5bf9aedb0ee40
Dtaitt <Donovan.taitt@yahoo.com>
2017-09-11 17:38:26 -0400

    fetched new stories

--- src/components/StoryFilter.jsx
+++ src/components/StoryFilter.jsx
@@ -3,35 +3,34 @@ import { FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 
 export default class StoryFilter extends Component {
 
-      state = {
-          value: "top",
-      }
+    state = {
+        value: "top",
+    }
 
-      handleChange = this.handleChange.bind(this);
-      handleSubmit = this.handleSubmit.bind(this);
+    handleChange = this.handleChange.bind(this);
+
+    //   componentDidMount() {
+    //     this.props.handleStoryFilter(this.state.value);
+    //   }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextState.value !== this.state.value;
+    }
     
-      handleChange(e) {
-        this.setState({value: e.target.value});
-      }
-    
-      handleSubmit(e) {
-        alert('Your favorite flavor is: ' + this.state.value);
-        e.preventDefault();
-      }
+    handleChange(e) {
+        this.setState({
+            value: e.target.value
+        }, () => { this.props.handleStoryFilter(this.state.value); });
+    }
 
     render() {
         return(
-            // <FormGroup controlId = "formControlsSelect" onSubmit = { this.handleSubmit }>
-            //     <ControlLabel>Pick your stories</ControlLabel>
-            //     <FormControl value = { this.state.value } onChange = { this.handleChange }>
-            //         <option value = "top">Top Stories</option>
-            //         <option value = "new">New Stories</option>
-            //         <option value = "best">Best Stories</option>
-            //     </FormControl>
-            // </FormGroup>
-            <FormGroup controlId="formControlsSelect" onSubmit = { this.handleSubmit }>
+            <FormGroup controlId="formControlsSelect">
                 <ControlLabel>Pick your stories</ControlLabel>
-                <FormControl componentClass="select" onChange = { this.handleChange }>
+                <FormControl 
+                    componentClass="select" 
+                    onChange = { this.handleChange }
+                >
                     <option value = "top">Top Stories</option>
                     <option value = "new">New Stories</option>
                     <option value = "best">Best Stories</option>
--- src/components/StoryListContainer.jsx
+++ src/components/StoryListContainer.jsx
@@ -4,20 +4,44 @@ import moment from 'moment';
 import StoryList from './StoryList';
 import StoryFilter from './StoryFilter';
 
+//may not need filterValue state. should rerender when storyfilter state.value changes
+//move handleStoryFilter from componentDidMount
+
 class StoryListContainer extends Component {
         
     state = {
         topStories: [],
         newStories: [],
+        filterValue: 'top',
     }
     
+    handleStoryFilter = this.handleStoryFilter.bind(this);
+    fetchStories = this.fetchStories.bind(this);
 
     componentDidMount() {
-        this.fetchTopStories();
+        this.handleStoryFilter();
     }
 
-    fetchTopStories() {
-        fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
+    handleStoryFilter(fValue) {
+        console.log(`${fValue} is what handleStoryFilter sees`);
+        
+        this.setState({
+            filterValue: fValue,
+        }, () => {
+            console.log(`${this.state.filterValue} is the filterValue state`)
+        })
+        
+        switch(fValue) {
+            case 'new':
+                this.fetchStories('new');
+                break;
+            default:
+                this.fetchStories('top')
+        }
+    }
+
+    fetchStories(storyType) {
+        fetch(`https://hacker-news.firebaseio.com/v0/${storyType}stories.json?print=pretty`)
             .then(res => {
                 return res.json()
             })
@@ -43,7 +67,7 @@ class StoryListContainer extends Component {
         
         return(
             <div>
-            <StoryFilter />
+            <StoryFilter handleStoryFilter = { this.handleStoryFilter } fetchStories = { this.fetchStories } />
             <StoryList topStories = { this.state.topStories } /> 
             </div>     
         );
