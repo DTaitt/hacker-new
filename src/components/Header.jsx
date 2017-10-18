38c30ff90947c9f92a73cd24231868f17f7fe602
Dtaitt <donovan.taitt@yahoo.com>
2017-10-18 10:31:15 -0400

    styled mobile and tablet view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,22 +5,24 @@ import { LinkContainer } from 'react-router-bootstrap';
 
 export default function Header() {
     return(
-        <Navbar>
-            <Navbar.Header>
-                <Navbar.Brand>
-                <Link to="/">
-                    <h1>HackerNew</h1>                       
-                </Link>
-                </Navbar.Brand>    
-            </Navbar.Header>
-            <Nav>
-                <LinkContainer to="/favorite">
-                    <NavItem eventKey = { 1 } >Favorite</NavItem>
+        <header>
+            <Navbar>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                    <Link to="/">
+                        <h1>HackerNew</h1>                       
+                    </Link>
+                    </Navbar.Brand>    
+                </Navbar.Header>
+                <Nav>
+                    <LinkContainer to="/favorite">
+                        <NavItem eventKey = { 1 } >Favorite</NavItem>
+                    </LinkContainer>
+                    <LinkContainer to="/history">
+                    <NavItem eventKey = { 2 } >History</NavItem>
                 </LinkContainer>
-                <LinkContainer to="/history">
-                <NavItem eventKey = { 2 } >History</NavItem>
-            </LinkContainer>
-            </Nav>
-        </Navbar>
+                </Nav>
+            </Navbar>
+        </header>
     )
 };
